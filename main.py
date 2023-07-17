ad110f2ad173b6d7d9b760664c66c6a60923b504
xavi <xpeiro92@gmail.com>
2023-07-17 11:52:24 +0200

    rm old code

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import time
 from functools import partial
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-from elements_paths import LoginElements, JobsElements
+import scanners.actions as scanners_actions
 from infraestracture.notifications.fs import FileSystemNotificator
-from job_url_builder import UrlGenerator, SalaryCodes, LocationCodes, RemoteCodes
-from messaging import print_error, print_relevant_info
+from job_url_builder import SalaryCodes, LocationCodes, RemoteCodes
+from messaging import print_relevant_info
 from openai_api import OpenAIClient
 from scanners.linkedin import LinkedinStates, Linkedin, JobsFilter
-from scanners.utilities import element_exists
-import scanners.actions as scanners_actions
 
 
 def init_bot() -> WebDriver:
@@ -27,45 +23,6 @@ def init_bot() -> WebDriver:
     driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     return driver
-
-
-def do_login(chrome, email, pw):
-    try:
-        # TODO P4: Move URL, no magic strings
-        chrome.get("https://www.linkedin.com/login?trk=guest_homepage-basic_nav-header-signin")
-        chrome.find_element("id", "username").send_keys(email)
-        time.sleep(3)
-        chrome.find_element("id", "password").send_keys(pw)
-        time.sleep(3)
-        chrome.find_element("xpath", LoginElements.submit_btn_xpath).click()
-    except Exception as e:
-        print_error("Couldn't log in Linkedin! ☠ Check if any intermediate screen appeared and credentials.")
-        raise e
-
-
-# TODO P2: Split navigation and parse/actions
-# TODO P1: Use config files
-def jobs_actions(chrome: WebDriver, start: int = 0):
-    url: str = UrlGenerator().generate(start=start)
-    chrome.get(url)
-    time.sleep(3)
-
-    if element_exists(chrome=chrome, find_arguments=(By.CLASS_NAME, JobsElements.no_results_class)):
-        # TODO P3: raise a finish Exception
-        return
-
-    time.sleep(4.4)
-
-    job_cards = chrome.find_elements(By.XPATH, JobsElements.all_job_cards_xpath)
-    print_relevant_info(f"scanning {len(job_cards)} elements from page {start // 25 + 1}")
-    for index, job_card in enumerate(job_cards):
-        job_card.click()
-        time.sleep(3)
-        job_title = job_card.find_element(By.CLASS_NAME, "job-card-list__title")
-        print_relevant_info(f"{index} - {job_title.text}")
-    else:
-        # next page
-        jobs_actions(chrome=chrome, start=start+25)
 
 
 def main():
