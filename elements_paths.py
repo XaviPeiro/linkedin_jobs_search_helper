7dee3b1e34216937fb608c70adcb4de686fdf78d
XaviPeiro <xpeiro92@gmail.com>
2025-11-12 15:51:13 +0100

    Update elements paths (website chnged)

--- elements_paths.py
+++ elements_paths.py
@@ -6,9 +6,8 @@ class ElementsPaths(ABC):
 
 
 class LoginElements(ElementsPaths):
-    submit_btn_xpath: str = '//*[@id="organic-div"]/form/div[3]/button'
-
-
+    submit_btn_xpath: str = '//button[@data-litms-control-urn="login-submit"]'
+    
 class JobsElements(ElementsPaths):
     pages_list_class: str = "artdeco-pagination__pages"
     no_results_class: str = "jobs-search-no-results-banner"
@@ -17,7 +16,7 @@ class JobsElements(ElementsPaths):
     all_not_dismissed_job_cards_xpath: str = "//li[@data-occludable-job-id and not(.//div[contains(@class, 'ob-card-list--is-dismissed')])]"
     discard_selected_job_css: str = ".jobs-search-results-list__list-item--active div.job-card-list__dismiss > button"
     selected_job_css: str = ".jobs-search-results-list__list-item--active"
-    job_card_title_css: str = ".job-card-list__title"
+    job_card_title_css: str = ".job-details-jobs-unified-top-card__job-title"
     discarded_job_card_css = ".job-card-list--is-dismissed"
     selected_job_card_title_css: str = selected_job_css + " " + job_card_title_css
     # job
