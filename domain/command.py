69ea139f27a1e816f3ffc83411adb08ba9d68b93
Xavi PL <xpeiro92@gmail.com>
2026-04-28 21:36:27 +0200

    refactor

--- domain/command.py
+++ domain/command.py
@@ -1,19 +1,12 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import ClassVar, Any
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-from domain.criteria import ICriteria
-from domain.notifier import Notifier
 from elements_paths import JobsElements
-from infraestracture.persistance.file_persistance import Job, FilePersistence
-import logging
-
-logger = logging.getLogger(__file__)
 
 class CrawlerReceiver(ABC):
     # net_navigator: Any
@@ -84,83 +77,3 @@ class SeleniumReceiver(CrawlerReceiver):
 
     def get_job_id(self) -> str:
         return self.net_navigator.find_element(By.CSS_SELECTOR, JobsElements.selected_job_css).get_attribute("data-job-id")
-
-
-
-@dataclass
-class LinkedinDiscardJobCommand(Command):
-    criteria: list[ICriteria]
-    net_navigator: CrawlerReceiver
-    notifier: Notifier
-    _action_name: ClassVar[str] = "DISCARD JOB"  # field(init=False, default="DISCARD JOB")
-
-    def __str__(self):
-        return f"Action: {self._action_name}."
-
-    """
-        Isn't possible to manually undo the "discard" action, so the btn only appears after clicking discard.
-        However, it can be done by requesting to the proper endpoint.
-    """
-    def __call__(self):
-        logger.info(f"Executing {str(self)} for {self.net_navigator.get_job_title()}")
-        # TODO: Will be nice to have track of processed elements in order to repeat the same analysis
-        #  over the already processed element thus reducing the time it takes and reqs to openai.
-        #  !!!NOT IN THIS FUNCTION.!!!!
-
-        job_descr = self.net_navigator.get_job_description()
-        # TODO: "criteria" is used as JobDescriptionOAICriteria.criteria (class and yaml) as the different algs to
-        #  to discard (JobDescriptionOAICriteria is ne of them, JobDescriptionOAICriteria). CONFUSING.
-        for criteria in self.criteria:
-            answer: [bool, None] = criteria.apply(entities=[job_descr])[0]
-            if answer is True:
-                self.net_navigator.linkedin_discard_job()
-                logger.info("DISCARDED")
-            elif answer is False:
-                logger.info("NOT DISCARDED")
-            else:
-                # The model is returning an unexpected message.
-                logger.info("NOT DISCARDED - Because unexpected answer from OPENAI.")
-
-
-"""
-You will be provided with IT job descriptions and you will be asked many questions about it in order to know if I should discard this job offer or not. 
-
-For each list of questions you receive from a job offer do the following:
-1. - Evaluate every single question independently, ie, check if they would get a positive or negative answer. If you are not sure about how a question should be evaluated, assume it will have a negative answer.
-2. - If the answer to one of the previously evaluated questions is positive, respond a single "yes", if any of the previous questions has 
- a negative answer, respond "no".
-
-Remember to answer a single "yes" or "no", according to the rules defined above.
-"""
-
-"""
-You will be provided with IT job descriptions and you will be asked many questions about it in order to know if I should discard this job offer or not. Do not guess, be strict and stick to the job description.
-
-For each list of questions you receive from a job offer do the following:
-1. - Evaluate every single question independently, ie, check if they would get a positive or negative answer. If you are not sure about how a question should be evaluated, assume it will have a negative answer.
-
-2. - Finally, write "yes" if the answer to any of them is yes, otherwise write "no".
-
-Remember to answer a single "yes" or "no", according to the rules defined above.
-"""
-
-@dataclass
-class PersistDataCommand(Command):
-    net_navigator: CrawlerReceiver
-    _action_name: ClassVar[str] = "Persist Jobs"
-    persistence: FilePersistence
-
-    def __str__(self):
-        return f"Action: {self._action_name}."
-
-    def __call__(self, *args, **kwargs):
-        logger.info(f"Executing {str(self)} for {self.net_navigator.get_job_title()}")
-        job: Job = Job.model_validate_strings(
-            {
-                'id': self.net_navigator.get_job_id(),
-                'url': self.net_navigator.get_url(),
-                'description': self.net_navigator.get_job_description(),
-                'title': self.net_navigator.get_job_title()
-            }
-        )
-        self.persistence(job)
