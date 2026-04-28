bba63b443177192fe472d2bdeefe8c7cb9bb3979
Xavi PL <xpeiro92@gmail.com>
2026-04-28 21:36:27 +0200

    refactor

---
+++ domain/persist_data_command/command.py
@@ -0,0 +1,30 @@
+import logging
+from dataclasses import dataclass
+from typing import ClassVar
+
+from domain.command import Command, CrawlerReceiver
+from infraestracture.persistance.file_persistance import FilePersistence, Job
+
+logger = logging.getLogger(__file__)
+
+
+@dataclass
+class PersistDataCommand(Command):
+    net_navigator: CrawlerReceiver
+    _action_name: ClassVar[str] = "Persist Jobs"
+    persistence: FilePersistence
+
+    def __str__(self):
+        return f"Action: {self._action_name}."
+
+    def __call__(self, *args, **kwargs):
+        logger.info(f"Executing {str(self)} for {self.net_navigator.get_job_title()}")
+        job: Job = Job.model_validate_strings(
+            {
+                "id": self.net_navigator.get_job_id(),
+                "url": self.net_navigator.get_url(),
+                "description": self.net_navigator.get_job_description(),
+                "title": self.net_navigator.get_job_title(),
+            }
+        )
+        self.persistence(job)
